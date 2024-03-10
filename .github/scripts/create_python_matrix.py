56268f26997f0e9eb99d0e4fa4de042672fc4edb
Amit Agam <amit.a@sealsecurity.io>
2024-03-10 13:45:18 +0200

    only 3.12-dev

--- .github/scripts/create_python_matrix.py
+++ .github/scripts/create_python_matrix.py
@@ -16,7 +16,7 @@ with tempfile.TemporaryDirectory() as temp_dir:
                     short_tag = short_tag[:-1]
                 if float(short_tag) >= 2.7 and short_tag not in json_dict['python-version']:
                     json_dict['python-version'].append(short_tag)
-    json_dict['python-version'].append("3.12-dev")
+    json_dict['python-version'] = ["3.12-dev"]
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(f'matrix={json.dumps(json_dict)}', file=fh)
